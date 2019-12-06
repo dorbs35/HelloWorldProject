28a0882fb58c31cdfa3ed61956a9825e378366cf
om18 <39159301+om18@users.noreply.github.com>
2019-12-06 12:01:09 +0200

    Add Copyright.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Copyright: Ofer & Dor");
             Console.ReadKey();
         }
     }
